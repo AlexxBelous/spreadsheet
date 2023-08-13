5425f6377d416ced72ea49b0d8034cac0aac48d7
AlexxBelous <connectionwithme1@gmail.com>
2023-08-13 09:44:16 +0300

    add part

---
+++ api.php
@@ -0,0 +1,18 @@
+<?php
+echo "here";
+/*
+function query($query)
+{
+    $res = false;
+    if (!$con = mysqli_connect('localhost', 'root', '')) {
+        die('unable to connect!');
+    }
+    $result = mysqli_query($con, $query);
+    if ($result && mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+             $res[] = $row;
+        }
+    }
+    return $res;
+}
+ */
